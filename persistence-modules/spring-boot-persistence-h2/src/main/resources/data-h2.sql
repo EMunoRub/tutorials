34d351dbf52136d31abb4b610e71f7f1bade17ad
EMunoRub <102967711+EMunoRub@users.noreply.github.com>
2022-04-07 13:09:18 +0200

    Update data-h2.sql

--- persistence-modules/spring-boot-persistence-h2/src/main/resources/data-h2.sql
+++ persistence-modules/spring-boot-persistence-h2/src/main/resources/data-h2.sql
@@ -3,3 +3,5 @@ INSERT INTO countries (id, name) VALUES (2, 'France');
 INSERT INTO countries (id, name) VALUES (3, 'Brazil');
 INSERT INTO countries (id, name) VALUES (4, 'Italy');
 INSERT INTO countries (id, name) VALUES (5, 'Canada');
+
+f
